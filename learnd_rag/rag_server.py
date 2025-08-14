db12635efa8d7d55e3cbd09c14c5d1defc6f97f1
JackieZhong00 <jackiery.zhong@gmail.com>
2025-08-14 02:43:16 -0700

    rag pipeline files

---
+++ learnd_rag/rag_server.py
@@ -0,0 +1,28 @@
+import grpc
+from concurrent import futures
+import event_dispatcher_pb2
+import event_dispatcher_pb2_grpc
+from rag import *
+
+class EventDispatcherServicer(event_dispatcher_pb2_grpc.EventDispatcherServicer):
+    def dispatchCardUpdates(self, request, context):
+        print("Received request:")
+        print(request)
+        result = compile_graph(request)
+
+        return event_dispatcher_pb2.DispatchResultGRPC(
+            success = True,
+            question=result.question,
+            answer=result.answer
+        )
+
+def serve():
+    server = grpc.server(futures.ThreadPoolExecutor(max_workers=10))
+    event_dispatcher_pb2_grpc.add_EventDispatcherServicer_to_server(EventDispatcherServicer(), server)
+    server.add_insecure_port('[::]:50051')  # Match port used in Java client
+    server.start()
+    print("gRPC server started on port 50051")
+    server.wait_for_termination()
+
+if __name__ == '__main__':
+    serve()
